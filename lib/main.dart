9a23cc8e98e99789bf4a00aff4d23753c7489ae8
m7md3bbas <mhmdabass330@gmail.com>
2025-09-28 20:02:31 +0200

    - setup firebase in project - add fastlane in our project and make it ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:vcare/core/routing/app_router.dart';
 import 'package:vcare/doc_app.dart';
+import 'package:vcare/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(DocApp(appRouter: AppRouter()));
 }
